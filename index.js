a8f6593043e93059d85803201776f8d4fcbe65c3
github username <github email address>
2022-06-01 16:45:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let employee={name:`Caitlin M`,streetAddress: "123 Seamore St"}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+      return Object.assign({},employee,{[key]:value}) 
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+       employee[key]=value
+      return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+      let newArray=Object.assign({},employee) 
+      delete newArray[key]
+      return newArray
+}
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key){
+      delete employee[key]
+      return employee
+}
